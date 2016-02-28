71194a331195f2d967972c05034605a24378ac4b
Greta Kavaliauskaite <greta.kavaliauskaite@cohaesus.co.uk>
2016-02-28 22:55:50 +0000

    new brench for search

--- js/map.js
+++ js/map.js
@@ -19,6 +19,7 @@ function initMap() {
 
          var data = json.markers[i];
          var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+         
          marker = new google.maps.Marker({
             map: map,
             animation: google.maps.Animation.DROP,
@@ -34,9 +35,41 @@ function initMap() {
             });
         })(marker, data);
     }
-
-});
-    
+});  
 }
+
+//function Model() {
+  //Define stuff here to hold your information such as lists of markers, places, and other data you need
+//}
+//var MODEL = new Model();
+
+// function ViewModel(){
+//     var self = this;
+//     self.data = ko.observableArray(markers);
+//     self.filter = ko.observable('');
+//     self.search = function(value){
+//         self.markers.removeAll();
+//         for (var i=0; i<markers.length; i++){
+//          //   console.log(markers[]);
+//             self.data().push(markers[i]);
+//         };
+//     };
+
+// self.personMarkers = function() {
+//     for (var i = 0; i < self.markers().length; i++) {
+//     new addMarker(self.markers()[i].lat(), self.markers()[i].long(), self.people()[i].nameTitle());
+//     };
+//   };
+
+//   self.personMarkers();
+  
+//};
+
+
+//ko.applyBindings(new ViewModel());
+
+Really all you should be using knockout for is the list view, the search bar, and possibly the map markers.
+
+markers: visible or not in your filtering loop
 
 
